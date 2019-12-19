6b0f0e3939e1d1e915210688ca26458e8ef97d23
tomic <tomic>
2019-12-19 23:28:12 +0100

    Simple backend server

---
+++ 1-tic-tac-toe/backend/README.md
@@ -0,0 +1,8 @@
+# Backend
+
+A very simple backend server in JS.
+Run `node server.js` to start a server on `localhost:8000`.
+
+You can talk to the server via curl, i.e.
+`curl localhost:8000`
+
---
+++ 1-tic-tac-toe/backend/server.js
@@ -0,0 +1,22 @@
+// A super-simple web server storing some state across requests.
+const http = require('http');
+
+const PORT = 8000;
+
+// This variable will store some state across requests.
+// Nothing fancy.
+// Will reset upon shutdown.
+const state = {
+    counter: 0,
+};
+
+http.createServer(function (req, res) {
+  console.log("Received a request: ", req);
+
+  state.counter += 1;
+
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  res.end(JSON.stringify(state));
+}).listen(PORT);
+
+console.log(`Server running on localhost:${PORT}`);
